8d938d60df35214e443a8925fde021a785a6452f
Jeffry Allan <jeffryallan26032001@gmail.com>
2025-04-22 18:46:46 +0530

    my first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+
+
+
+//our first component
+
+//component reusable piece of ui
+
+function Header (){
+  return <h1>Below the world</h1>
+}
+
+
+
+
+
+
+
+
+
 function App() {
    return (
    <>
-   <h1>hello world!</h1>
+   <Header/>
+   <Header></Header>
+   <Header/>
+
+   <Header/>
+
+   <Header/>
+
    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At perferendis nisi minus reiciendis, similique odit blanditiis assumenda architecto, exercitationem pariatur id dolore, doloremque voluptatem voluptatibus? Facilis impedit in unde quae.</p>
    </>
    )
