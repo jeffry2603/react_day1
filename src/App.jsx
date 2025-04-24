ea6e27a8c1cf4c80dfa556b58b471c8e34a3e342
Jeffry Allan <jeffryallan26032001@gmail.com>
2025-04-24 22:03:07 +0530

    working with some examples and adding to the fucntion --chuma

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,30 @@
 import Header from "./components/Header";
 import Description from "./components/Description";
 
+
+
+
+//Base component
 function App() {
+
+   let a = 51;
+   let b = 47;
+
+   let c = a + b - b / a
+
+// let birthdate = new Date('1990-04-23')
+
+// let today = new Date();
+
+// let age = today.getFullYear() - birthdate.getFullYear()
+
+
+
    return (
    <>
+<div>
+the answer for the equation of {a} and {b} is {c} 
+ </div>
    <Header/>
 
    <Header/>
@@ -22,6 +43,10 @@ function App() {
 <Description />
 
 <Description />
+
+
+
+
 </>
    )
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 function Header (){
     return <h1>Below the world</h1>
